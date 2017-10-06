8d485a4c0d4d35d8b0a87391de57a5a392bb2c76
lizleano <lizleano35@gmail.com>
2017-10-06 16:37:34 -0400

    removed ignore files

---
+++ KeysTW.py
@@ -0,0 +1,5 @@
+Name,key
+TW_consumer_key,hEFgsrws8gBEHepxXNEovzuDj
+TW_consumer_secret,mNOQtuZImUEu03qrFwnG01HhfOPchnOYnDkX210ADbcM27IzO8
+TW_access_token,909939501295919104-PfIeAJdSLT0aLIHDvhP4tRccUlIORSL
+TW_access_token_secret,4QLCg8whxm7aLp9cjkr2DA73pTiDoUkbVxwbkvbS4RI7C
